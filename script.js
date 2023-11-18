8f25a2dd582c8c2a0b96030c5566be0320720fbc
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-11-18 22:22:06 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const hrt = document.querySelector('.heart-Circle');
+
+const toggle = function () {
+	if ( hrt.getAttribute('aria-checked') === 'true' ) {
+		hrt.setAttribute('aria-checked', 'false');
+	}
+	else {
+		hrt.setAttribute('aria-checked', 'true');
+	}
+}
+
+hrt.addEventListener('click', function() {
+	toggle();
+	document.getElementById('label2').style.display = 'block';
+  }, false);
+hrt.addEventListener('keypress', function ( e ) {
+	const keyCode = e.keyCode || e.which;
+	switch ( keyCode ) {
+		case 32:
+			e.preventDefault();
+			toggle();
+			break;
+		default:
+			break;
+	}
+
+
+})
+	
